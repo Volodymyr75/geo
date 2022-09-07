6b3a650e6da7852796883c3e5fcd497511e70e45
Volodymyr Strembovskyi <strembov@gmail.com>
2022-09-07 17:37:09 +0300

    first API call

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,29 @@
-function App() {
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/Header'
+import Search from './components/Search'
+
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN
+
+const App = () => {
+  const [word, setWord] = useState('')
+  const handleSearchSubmit = (e) => {
+    e.preventDefault()
+    // console.log(word)
+    fetch(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${word}.json?access_token=${MAPBOX_TOKEN}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }
+
   return (
-    <div className="App">
-      <h1>Geo</h1>
+    <div>
+      <Header title="Geo app" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   )
 }
---
+++ front/src/components/Search.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
+const Search = ({ word, setWord, handleSubmit }) => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col xs={9}>
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Enter place"
+                />
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Search
