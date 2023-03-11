b29508d5e905b743590ffb4c8b108e81802d55c1
Volodymyr Strembovskyi <strembov@gmail.com>
2023-03-11 15:29:35 +0200

    add gunicorn

--- api/manage.py
+++ api/manage.py
@@ -9,6 +9,6 @@ from service_api.app import app
 # port = os.environ.get('PORT', 5050)
 
 if __name__ == '__main__':
-    # app.run(port=5050)
-    port = os.environ.get("PORT", 5050)
-    app.run(debug=False, host='0.0.0.0', port=port) # for deployment
+    app.run(port=5050)
+    # port = os.environ.get("PORT", 5050)
+    # app.run(debug=False, host='0.0.0.0', port=port) # for deployment
